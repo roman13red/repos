a64a392cb9df1e7dacbe11ca884d57b2eb2ccab4
роман куткин <kutkinroman@mail.ru>
2019-08-09 18:49:15 +0300

    eat

--- algoritmTest/Program.cs
+++ algoritmTest/Program.cs
@@ -5,31 +5,26 @@ namespace algoritmTest
 {
     class Program
     {
-        static  int[][] Function(int[] values, int result)
-        { int Ind = 0;
-            int[][] otvet = new int[values.Length- 1][];
+        static  int[] Function(int[] values, int result)
+        {
+            int[] otvet = new int[1];
             foreach (var b in values)
             {
                 var chislo2 = result - b;
                 var num = Array.IndexOf(values, chislo2);
                 if (num != -1)
                 {
-                    var mass = new int[2] { Array.IndexOf(values, b), num }.OrderBy(x=>x).ToArray();
+                     otvet = new int[2] { Array.IndexOf(values, b), num }.OrderBy(x=>x).ToArray();
 
-                    if (Array.IndexOf(otvet, mass) == -1)
-                    {
-                        otvet[Ind] = mass;
-                        Ind++;
-                    }
+                    break;
                 }
 
 
 
             }
-
-            otvet = otvet.Where(x => x != null).ToArray();
-
-                return (otvet);
+            
+            
+            return (otvet);
         }
 
         static void Main(string[] args)
@@ -38,8 +33,8 @@ namespace algoritmTest
             var otvet = Function(number,sum);
             foreach (var f in otvet)
             {
-                foreach (var b in f) Console.Write($"{b} ");
-                Console.WriteLine();
+                 Console.Write($"{f} ");
+                
             }
         }
     }
